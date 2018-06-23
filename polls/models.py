a64ae872039328eadde4e7675cc894cff0e6be6b
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-06-23 18:34:46 +0700

    Add to String method

--- polls/models.py
+++ polls/models.py
@@ -5,8 +5,15 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return '[question_text={}, pub_date={}]'.format(self.question_text, self.pub_date)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return '[question={}, choice_text={}, votes={}]'.format(self.question.question_text, self.choice_text,
+                                                                self.votes)
