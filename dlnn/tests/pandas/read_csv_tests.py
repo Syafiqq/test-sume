2da591b01e3a10765630638f8014288c8820568e
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-06-24 22:45:22 +0700

    Finishing test_reshaping_feature

--- dlnn/tests/pandas/read_csv_tests.py
+++ dlnn/tests/pandas/read_csv_tests.py
@@ -2,6 +2,7 @@ import os
 from unittest import TestCase
 
 import numpy
+import numpy.matlib
 import pandas
 
 from sumeq.settings import BASE_DIR
@@ -51,3 +52,17 @@ class ReadCSVTest(TestCase):
         feature *= 1 / 300.0
         self.assertTrue(numpy.allclose([.8433, .4367, .8633, .3167], feature[0], rtol=1e-3))
         # print(feature)
+
+    def test_reshaping_feature(self):
+        dataframe = pandas.read_csv(self.corpus_path, header=None)
+        dim = dataframe.shape
+        r_feature = dataframe.drop(columns=dim[1] - 1)
+        feature = r_feature.values.astype(float)
+        feature_dim = numpy.shape(feature)
+        feature *= 1 / 300.0
+        nfeature = numpy.delete(numpy.empty([1, feature_dim[1], feature_dim[1]]), 0, 0)
+        for _k, _v in enumerate(feature):
+            nfeature = numpy.concatenate((nfeature, [numpy.matlib.repmat(_v, feature_dim[1], 1)]))
+        oneliner = numpy.tile(numpy.reshape(feature, (feature_dim[0], 1, feature_dim[1])), (1, feature_dim[1], 1))
+        self.assertTrue(numpy.allclose(nfeature, oneliner, rtol=1e-3))
+        # print(oneliner)
