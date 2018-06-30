b634afcd08ef974469f5183eaf9fddb620a389f4
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-06-30 21:14:52 +0700

    Initial commit of CustomConv2D

---
+++ dlnn/layer/Conv2D.py
@@ -0,0 +1,15 @@
+from keras.layers import Conv2D as c2D
+
+
+class Conv2D(c2D):
+    def __init__(self, **kwargs):
+        super(Conv2D, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        super(Conv2D, self).build(input_shape)  # Be sure to call this at the end
+
+    def call(self, x):
+        return super(Conv2D, self).call(x)
+
+    def compute_output_shape(self, input_shape):
+        return super(Conv2D, self).compute_output_shape(input_shape)
