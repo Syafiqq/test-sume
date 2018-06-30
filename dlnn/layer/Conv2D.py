c8635d1981c74ec61594112b6d7f99343f1f2fd1
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-06-30 21:34:19 +0700

    Add dummy operation

--- dlnn/layer/Conv2D.py
+++ dlnn/layer/Conv2D.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from keras.layers import Conv2D as c2D
 
 
@@ -10,7 +11,7 @@ class Conv2D(c2D):
         super(Conv2D, self).build(input_shape)  # Be sure to call this at the end
 
     def call(self, x):
-        return super(Conv2D, self).call(x)
+        return tf.tile(x, [1, self.filters, 1, 1]) * 0
 
     def compute_output_shape(self, input_shape):
         return super(Conv2D, self).compute_output_shape(input_shape)
