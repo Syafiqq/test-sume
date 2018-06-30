c3e6e2a4a88c18cae6f256b4bbc8d61c46285271
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-06-30 21:33:57 +0700

    Capture filters

--- dlnn/layer/Conv2D.py
+++ dlnn/layer/Conv2D.py
@@ -2,8 +2,9 @@ from keras.layers import Conv2D as c2D
 
 
 class Conv2D(c2D):
-    def __init__(self, **kwargs):
-        super(Conv2D, self).__init__(**kwargs)
+    def __init__(self, filters, **kwargs):
+        self.filters = filters
+        super(Conv2D, self).__init__(filters, **kwargs)
 
     def build(self, input_shape):
         super(Conv2D, self).build(input_shape)  # Be sure to call this at the end
