3d4f515cbb5ca3b8092a65f3c385cb0edd5fdfe4
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-07-01 19:21:28 +0700

    Add iterate trough tensor

--- dlnn/layer/Conv2D.py
+++ dlnn/layer/Conv2D.py
@@ -11,14 +11,19 @@ class Conv2D(c2D):
         super(Conv2D, self).build(input_shape)  # Be sure to call this at the end
 
     def call(self, x):
+        import tensorflow as tf
         shape_x = x.shape
         if shape_x[1] > 1 and shape_x[1] != self.filters:
             raise ValueError('Cannot Operate Convolution')
 
         if shape_x[1] == 1 and shape_x[1] != self.filters:
             x = K.tile(x, (1, self.filters, 1, 1))
-            
-        return x
+
+        def calls(y):
+            return y + 10
+
+        zz = tf.map_fn(calls, x[:])
+        return zz
 
     def compute_output_shape(self, input_shape):
         return super(Conv2D, self).compute_output_shape(input_shape)
