3954852f0c636b91187c48fc4601f4418b024090
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-07-01 13:01:45 +0700

    Expanding value to the output value

--- dlnn/layer/Conv2D.py
+++ dlnn/layer/Conv2D.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
+from keras import backend as K
 from keras.layers import Conv2D as c2D
 
 
@@ -11,7 +11,14 @@ class Conv2D(c2D):
         super(Conv2D, self).build(input_shape)  # Be sure to call this at the end
 
     def call(self, x):
-        return tf.tile(x, [1, self.filters, 1, 1]) * 0
+        shape_x = x.shape
+        if shape_x[1] > 1 and shape_x[1] != self.filters:
+            raise ValueError('Cannot Operate Convolution')
+
+        if shape_x[1] == 1 and shape_x[1] != self.filters:
+            x = K.tile(x, (1, self.filters, 1, 1))
+            
+        return x
 
     def compute_output_shape(self, input_shape):
         return super(Conv2D, self).compute_output_shape(input_shape)
