7ea412f50e049d92fec31aa62f03684222f87c91
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-07-08 14:42:08 +0700

    Add test_activation_1

--- dlnn/tests/ml/activation_test.py
+++ dlnn/tests/ml/activation_test.py
@@ -10,3 +10,12 @@ class ActivationTest(TestCase):
         x = act.call(K.variable([0.2]))
         self.assertIsNotNone(x)
         # print(K.eval(x))
+
+    def test_activation_1(self):
+        from keras.layers import Activation
+        act = Activation('sigmoid')
+        from dlnn.tests.ml.avg_conv_test import initial_result
+        from dlnn.tests.ml.avg_conv_test import AvgConvTest
+        x = act.call(initial_result(AvgConvTest().corpus()))
+        self.assertIsNotNone(x)
+        # print(K.eval(x))
