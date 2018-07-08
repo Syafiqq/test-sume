d1e23b6a381761d6e06924cd770a79840f172d41
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-07-08 15:14:10 +0700

    Add param testing

--- dlnn/tests/ml/repos_helper.py
+++ dlnn/tests/ml/repos_helper.py
@@ -1,3 +1,5 @@
+from unittest import TestCase
+
 import numpy
 
 normalized = numpy.array([
@@ -19,3 +21,25 @@ corr_step_1_seg_3 = numpy.array([[0.3667, 0.3957, 0.3375, 0.3661],
                                  [0.3653, 0.2085, 0.2488, 0.3782],
                                  [0.3667, 0.3957, 0.3375, 0.3661]])
 corr_step_1 = numpy.concatenate(([[corr_step_1_seg_1]], [[corr_step_1_seg_2]], [[corr_step_1_seg_3]]), axis=0)
+
+
+class ReposHelper(TestCase):
+    def test_normalize(self):
+        self.assertIsNotNone(normalized)
+        # print(normalized)
+
+    def test_corr_step_1_seg_1(self):
+        self.assertIsNotNone(corr_step_1_seg_1)
+        # print(corr_step_1_seg_1)
+
+    def test_corr_step_1_seg_2(self):
+        self.assertIsNotNone(corr_step_1_seg_2)
+        # print(corr_step_1_seg_2)
+
+    def test_corr_step_1_seg_3(self):
+        self.assertIsNotNone(corr_step_1_seg_3)
+        # print(corr_step_1_seg_3)
+
+    def test_corr_step_1(self):
+        self.assertIsNotNone(corr_step_1)
+        # print(corr_step_1)
