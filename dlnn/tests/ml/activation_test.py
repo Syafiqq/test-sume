ede93e5b1d22076e1b8769a6e9261a24d97bc091
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-07-10 06:47:23 +0700

    Add correction to test

--- dlnn/tests/ml/activation_test.py
+++ dlnn/tests/ml/activation_test.py
@@ -14,7 +14,10 @@ class ActivationTest(TestCase):
     def test_sigmoid_activation_from_corr_step_1(self):
         from keras.layers import Activation
         from dlnn.tests.ml.repos_helper import corr_step_1
+        from dlnn.tests.ml.repos_helper import corr_step_2
+        import numpy
         act = Activation('sigmoid')
         x = act.call(corr_step_1)
         self.assertIsNotNone(x)
+        self.assertTrue(numpy.allclose(K.eval(x), corr_step_2, rtol=1e-3))
         # print(K.eval(x))
