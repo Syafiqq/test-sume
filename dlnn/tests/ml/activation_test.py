8f3d990f4e7210b1de11afc2276dd25d01ec6cd8
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-07-10 06:57:59 +0700

    change call to layer_step_2

--- dlnn/tests/ml/activation_test.py
+++ dlnn/tests/ml/activation_test.py
@@ -17,44 +17,40 @@ class ActivationTest(TestCase):
         # print(K.eval(x))
 
     def test_sigmoid_activation_from_corr_step_1_seg_1(self):
-        from keras.layers import Activation
         from dlnn.tests.ml.repos_helper import corr_step_1_seg_1
         from dlnn.tests.ml.repos_helper import corr_step_2_seg_1
         import numpy
-        act = Activation('sigmoid')
+        act = layer_step_2()
         x = act.call(corr_step_1_seg_1)
         self.assertIsNotNone(x)
         self.assertTrue(numpy.allclose(K.eval(x), corr_step_2_seg_1, rtol=1e-3))
         # print(K.eval(x))
 
     def test_sigmoid_activation_from_corr_step_1_seg_2(self):
-        from keras.layers import Activation
         from dlnn.tests.ml.repos_helper import corr_step_1_seg_2
         from dlnn.tests.ml.repos_helper import corr_step_2_seg_2
         import numpy
-        act = Activation('sigmoid')
+        act = layer_step_2()
         x = act.call(corr_step_1_seg_2)
         self.assertIsNotNone(x)
         self.assertTrue(numpy.allclose(K.eval(x), corr_step_2_seg_2, rtol=1e-3))
         # print(K.eval(x))
 
     def test_sigmoid_activation_from_corr_step_1_seg_3(self):
-        from keras.layers import Activation
         from dlnn.tests.ml.repos_helper import corr_step_1_seg_3
         from dlnn.tests.ml.repos_helper import corr_step_2_seg_3
         import numpy
-        act = Activation('sigmoid')
+        act = layer_step_2()
         x = act.call(corr_step_1_seg_3)
         self.assertIsNotNone(x)
         self.assertTrue(numpy.allclose(K.eval(x), corr_step_2_seg_3, rtol=1e-3))
         # print(K.eval(x))
 
     def test_sigmoid_activation_from_corr_step_1(self):
-        from keras.layers import Activation
         from dlnn.tests.ml.repos_helper import corr_step_1
         from dlnn.tests.ml.repos_helper import corr_step_2
         import numpy
-        act = Activation('sigmoid')
+        act = layer_step_2()
         x = act.call(corr_step_1)
         self.assertIsNotNone(x)
         self.assertTrue(numpy.allclose(K.eval(x), corr_step_2, rtol=1e-3))
