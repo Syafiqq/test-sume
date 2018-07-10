362c3dcb9adcc53bf561ddcdb93c149e3ee09fe7
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-07-10 23:30:03 +0700

    Add corr step 5 test

--- dlnn/tests/ml/cnn_test.py
+++ dlnn/tests/ml/cnn_test.py
@@ -8,12 +8,14 @@ class CnnTest(TestCase):
         from dlnn.tests.ml.activation_test import layer_step_2
         from dlnn.tests.ml.conv_test import layer_step_3
         from dlnn.tests.ml.activation_test import layer_step_4
+        from dlnn.tests.ml.pooling_test import layer_step_5
 
         model = Sequential()
         model.add(layer_step_1())
         model.add(layer_step_2())
         model.add(layer_step_3())
         model.add(layer_step_4())
+        model.add(layer_step_5(2))
         return model
 
     def test_input_to_step_1(self):
@@ -69,5 +71,19 @@ class CnnTest(TestCase):
         output = network.predict(normalized)
         self.assertIsNotNone(output)
         self.assertTrue(numpy.allclose(output, corr_step_4, rtol=1e-3))
+        # print(output)
+        # print(output.shape)
+
+    def test_input_to_step_5(self):
+        from keras import Model
+        from dlnn.tests.ml.repos_helper import normalized
+        from dlnn.tests.ml.repos_helper import corr_step_5
+        import numpy
+        model = self.build_helper()
+        network = Model(inputs=model.input,
+                        outputs=model.get_layer(index=4).output)
+        output = network.predict(normalized)
+        self.assertIsNotNone(output)
+        self.assertTrue(numpy.allclose(output, corr_step_5, rtol=1e-3))
         # print(output)
         # print(output.shape)
---
+++ dlnn/tests/ml/pooling_test.py
@@ -0,0 +1,50 @@
+from unittest import TestCase
+
+from keras import backend as K
+
+
+def layer_step_5(window_size):
+    from keras.layers import MaxPooling2D
+    return MaxPooling2D(pool_size=(window_size, window_size), padding='same', data_format='channels_first')
+
+
+class PoolingTest(TestCase):
+    def test_pooling_w2_from_corr_step_4_seg_1(self):
+        from dlnn.tests.ml.repos_helper import corr_step_4_seg_1
+        from dlnn.tests.ml.repos_helper import corr_step_5_seg_1
+        import numpy
+        act = layer_step_5(2)
+        x = act.call(K.variable(numpy.concatenate(([[[corr_step_4_seg_1]]]))))
+        self.assertIsNotNone(x)
+        self.assertTrue(numpy.allclose(K.eval(x)[0][0], corr_step_5_seg_1, rtol=1e-3))
+        # print(K.eval(x))
+
+    def test_pooling_w2_from_corr_step_4_seg_2(self):
+        from dlnn.tests.ml.repos_helper import corr_step_4_seg_2
+        from dlnn.tests.ml.repos_helper import corr_step_5_seg_2
+        import numpy
+        act = layer_step_5(2)
+        x = act.call(K.variable(numpy.concatenate(([[[corr_step_4_seg_2]]]))))
+        self.assertIsNotNone(x)
+        self.assertTrue(numpy.allclose(K.eval(x)[0][0], corr_step_5_seg_2, rtol=1e-3))
+        # print(K.eval(x))
+
+    def test_pooling_w2_from_corr_step_4_seg_3(self):
+        from dlnn.tests.ml.repos_helper import corr_step_4_seg_3
+        from dlnn.tests.ml.repos_helper import corr_step_5_seg_3
+        import numpy
+        act = layer_step_5(2)
+        x = act.call(K.variable(numpy.concatenate(([[[corr_step_4_seg_3]]]))))
+        self.assertIsNotNone(x)
+        self.assertTrue(numpy.allclose(K.eval(x)[0][0], corr_step_5_seg_3, rtol=1e-3))
+        # print(K.eval(x))
+
+    def test_pooling_w2_from_corr_step_4(self):
+        from dlnn.tests.ml.repos_helper import corr_step_4
+        from dlnn.tests.ml.repos_helper import corr_step_5
+        import numpy
+        act = layer_step_5(2)
+        x = act.call(K.variable(corr_step_4))
+        self.assertIsNotNone(x)
+        self.assertTrue(numpy.allclose(K.eval(x), corr_step_5, rtol=1e-3))
+        # print(K.eval(x))
