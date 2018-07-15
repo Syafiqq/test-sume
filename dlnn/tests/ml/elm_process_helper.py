0dd4734f7bf530f3b0ec1897c66ad5ffc5e8bd2a
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-07-15 19:36:21 +0700

    Add test_step_10_a_output test and step_10_a_dummy_bias_non_spread_init initializer

--- dlnn/tests/ml/elm_process_helper.py
+++ dlnn/tests/ml/elm_process_helper.py
@@ -13,6 +13,10 @@ def step_10_a_dummy_bias_init(shape, dtype=None):
     return K.variable(corr_step_10_a_dummy_bias_init)
 
 
+def step_10_a_dummy_bias_non_spread_init(shape, dtype=None):
+    return step_10_a_dummy_bias_init(shape, dtype)[0]
+
+
 class ElmProcessHelper(TestCase):
     def test_step_9_output(self):
         from dlnn.tests.ml.repos_helper import corr_step_8_full
@@ -73,6 +77,20 @@ class ElmProcessHelper(TestCase):
         from dlnn.tests.ml.repos_helper import corr_step_10_a
         import numpy
         x = K.dot(K.variable(corr_step_9), step_10_a_dummy_kernel_init(None)) + step_10_a_dummy_bias_init(None)
+        self.assertIsNotNone(x)
+        self.assertTrue(numpy.allclose(K.eval(x), corr_step_10_a, rtol=1e-6))
+        # print(K.eval(x))
+
+    def test_step_10_a_output(self):
+        from dlnn.tests.ml.repos_helper import corr_step_9
+        from dlnn.tests.ml.repos_helper import corr_step_10_a
+        from keras.layers import Dense
+        import numpy
+        corr_step_9 = K.variable(corr_step_9)
+        layer = Dense(5, activation=None, use_bias=True, kernel_initializer=step_10_a_dummy_kernel_init,
+                      bias_initializer=step_10_a_dummy_bias_non_spread_init, trainable=False)
+        layer.build(corr_step_9.shape)
+        x = layer.call(corr_step_9)
         self.assertIsNotNone(x)
         self.assertTrue(numpy.allclose(K.eval(x), corr_step_10_a, rtol=1e-6))
         # print(K.eval(x))
