1e41065f52a2a2fa7b2fe134e3814f8d6f170827
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-07-15 20:37:47 +0700

    test_input_to_step_11_a_dummy

--- dlnn/tests/ml/elm_func_test.py
+++ dlnn/tests/ml/elm_func_test.py
@@ -3,6 +3,7 @@ from keras import backend as K
 from keras.layers import Flatten, Dense
 from scipy import stats
 
+from dlnn.tests.ml.activation_test import layer_step_11_a
 from dlnn.tests.ml.cnn_func_test import inputs, step_8
 from dlnn.tests.ml.elm_process_helper import step_10_a_dummy_kernel_init, step_10_a_dummy_bias_non_spread_init
 from dlnn.tests.ml.repos_helper import normalized
@@ -25,6 +26,7 @@ def unifinv_init(shape, dtype=None):
 
 step_9 = layer_step_9()(step_8)
 step_10_a_dummy = layer_step_10_a_dummy()(step_9)
+step_11_a_dummy = layer_step_11_a()(step_10_a_dummy)
 
 
 class ElmFuncTest(TestCase):
@@ -49,5 +51,17 @@ class ElmFuncTest(TestCase):
         output = network.predict(normalized)
         self.assertIsNotNone(output)
         self.assertTrue(numpy.allclose(output, corr_step_10_a_dummy, rtol=1e-6))
+        # print(output)
+        # print(output.shape)
+
+    def test_input_to_step_11_a_dummy(self):
+        from keras import Model
+        from dlnn.tests.ml.repos_helper import corr_step_11_a_dummy
+        import numpy
+        network = Model(inputs=inputs, outputs=step_11_a_dummy)
+        network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+        output = network.predict(normalized)
+        self.assertIsNotNone(output)
+        self.assertTrue(numpy.allclose(output, corr_step_11_a_dummy, rtol=1e-6))
         # print(output)
         # print(output.shape)
