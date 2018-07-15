6d73139b10928f0fa46a43f586174e6d88defd1c
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-07-15 11:21:00 +0700

    Add test_input_to_step_9_0

--- dlnn/tests/ml/elm_func_test.py
+++ dlnn/tests/ml/elm_func_test.py
@@ -1,5 +1,20 @@
+from dlnn.tests.ml.cnn_func_test import inputs, step_8
+from dlnn.tests.ml.elm_process_helper import layer_step_9_0
+from dlnn.tests.ml.repos_helper import normalized
 from dlnn.tests.ml.testcase import TestCase
+
+step_9_0 = layer_step_9_0()(step_8)
 
 
 class ElmFuncTest(TestCase):
-    pass
+    def test_input_to_step_9_0(self):
+        from keras import Model
+        from dlnn.tests.ml.repos_helper import corr_step_9_flatten
+        import numpy
+        network = Model(inputs=inputs, outputs=step_9_0)
+        network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+        output = network.predict(normalized)
+        self.assertIsNotNone(output)
+        self.assertTrue(numpy.allclose(output, corr_step_9_flatten, rtol=1e-6))
+        # print(output)
+        # print(output.shape)
