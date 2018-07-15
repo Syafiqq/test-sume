4c8812ca01c46ba2107e6f28ec8297f9255da25a
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-07-15 11:17:29 +0700

    Add test_step_0_flatten_data test

--- dlnn/tests/ml/elm_process_helper.py
+++ dlnn/tests/ml/elm_process_helper.py
@@ -8,4 +8,12 @@ def layer_step_9_0():
 
 
 class ElmProcessHelper(TestCase):
-    pass
+    def test_step_0_flatten_data(self):
+        from dlnn.tests.ml.repos_helper import corr_step_8_full
+        from dlnn.tests.ml.repos_helper import corr_step_9_flatten
+        import numpy
+        layer = layer_step_9_0()
+        x = layer.call(corr_step_8_full)
+        self.assertIsNotNone(x)
+        self.assertTrue(numpy.allclose(K.eval(x), corr_step_9_flatten, rtol=1e-6))
+        # print(K.eval(x))
