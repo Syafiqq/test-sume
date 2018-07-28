64a07e154759f8c0349a2abe4a27ef1e8e33888a
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-07-28 18:49:29 +0700

    Add some method

--- dlnn/Dlnn.py
+++ dlnn/Dlnn.py
@@ -1,2 +1,16 @@
 class Dlnn(object):
-    pass
+    @staticmethod
+    def config(*args, **kwargs):
+        return Dlnn()
+
+    def train(self, x, y):
+        self._train(x, y)
+        return self._evaluate(x, y)
+
+    def _train(self, x, y):
+        # TODO : Place Training Process Here
+        pass
+
+    def _evaluate(self, x, y):
+        # TODO : Place Evaluation Process Here
+        pass
