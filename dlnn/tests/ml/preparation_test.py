443223a38300f86db77345ce2954dae0b68cb5ea
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-07-29 12:03:16 +0700

    Add test_separate_data_and_its_label

--- dlnn/tests/ml/preparation_test.py
+++ dlnn/tests/ml/preparation_test.py
@@ -1,5 +1,14 @@
+import numpy as np
+
+from dlnn.tests.ml.repos_helper import corpus, corpus_data, corpus_label
 from dlnn.tests.ml.testcase import TestCase
 
 
 class PreparationTest(TestCase):
-    pass
+    def test_separate_data_and_its_label(self):
+        combined = corpus
+        _corpus_data, _corpus_label = np.hsplit(combined, [combined.shape[1] - 1])
+        self.assertTrue(np.allclose(_corpus_data, corpus_data, rtol=0))
+        self.assertTrue(np.allclose(_corpus_label, corpus_label, rtol=0))
+        # print(corpus_data, corpus_label)
+        self.assertIsNotNone(combined)
