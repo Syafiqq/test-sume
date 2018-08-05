861721eaa63e87b2eb3b704cc49f3b6203f7ad8d
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-08-05 11:13:47 +0700

    Override Dlnn build model

--- dlnn/tests/ml/dlnn_func_test.py
+++ dlnn/tests/ml/dlnn_func_test.py
@@ -2,8 +2,15 @@ import keras
 import numpy
 from keras import Input
 from keras import backend as K
+from keras.engine.saving import load_model
+from keras.initializers import Initializer
 from keras.layers import Lambda, Reshape
 
+from dlnn.Dlnn import Dlnn
+from dlnn.layer.Conv2D import AvgFilter, MaxFilter, StdDevFilter, Conv2D
+from dlnn.layer.MergeCategorical import MergeCategorical
+from dlnn.layer.Scaling import Scaling
+from dlnn.layer.Tiling import Tiling
 from dlnn.tests.ml.activation_test import layer_2_activation, layer_4_activation, layer_7_activation, \
     layer_11_a_activation, layer_11_b_activation, layer_11_c_activation
 from dlnn.tests.ml.conv_test import layer_1_conv, layer_3_conv, layer_6_conv
@@ -12,9 +19,12 @@ from dlnn.tests.ml.elm_func_test import layer_9_flatten, layer_10_a_dense, layer
     layer_15_merge_categorical
 from dlnn.tests.ml.pooling_test import layer_5_pool, layer_8_pool
 from dlnn.tests.ml.repos_helper import corpus_data, label_init, corpus_label, normalized, corr_step_1, corr_step_2, \
-    corr_step_3, corr_step_4, corr_step_5, corr_step_6, corr_step_7, corr_step_8, corr_step_8_full, corr_step_9
+    corr_step_3, corr_step_4, corr_step_5, corr_step_6, corr_step_7, corr_step_8, corr_step_8_full, corr_step_9, \
+    corr_step_10_a_bias_init, corr_step_10_a_kernel_init, corr_step_10_b_kernel_init, corr_step_10_b_bias_init, \
+    corr_step_10_c_bias_init, corr_step_10_c_kernel_init
 from dlnn.tests.ml.testcase import TestCase
 from dlnn.util import to_categorical
+from dlnn.util.Initializers import Unifinv
 
 inputs = Input(shape=(corpus_data.shape[-1],))
 scale = Lambda(lambda x: x * 1.0 / 300.0)(inputs)
@@ -41,6 +51,216 @@ step_12_c_dense = layer_12_c_dense()(step_11_c_activation)
 step_13_concatenate = layer_13_concatenate()([step_12_a_dense, step_12_b_dense, step_12_c_dense])
 step_14_reshape = layer_14_reshape()(step_13_concatenate)
 step_15_output = layer_15_merge_categorical()(step_14_reshape)
+
+
+class CustomInitializer(Initializer):
+    """Initializer that generates tensors initialized to 0.
+    """
+
+    def __init__(self, val) -> None:
+        self.val = val
+        super(Initializer, self).__init__()
+
+    def __call__(self, shape, dtype=None):
+        return K.variable(self.val)
+
+    def get_config(self):
+        config = {
+            'val': self.val,
+        }
+        base_config = super(CustomInitializer, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    @classmethod
+    def from_config(cls, config):
+        if 'val' in config:
+            config['val'] = numpy.array(config['val']['value'])
+
+        return super().from_config(config)
+
+
+class DlnnDump(Dlnn):
+
+    def __init__(self, network_name='network-dump', scale_min1=0.0, scale_max1=0.0, scale_min2=0.0, scale_max2=0.0,
+                 conv_1_window=0, conv_2_window=0, conv_3_window=0, pool_1_size=0, pool_2_size=0, elm_1_dense_1_units=0,
+                 elm_2_dense_1_units=0, elm_3_dense_1_units=0, elm_1_dense_1_kernel_min=0.0,
+                 elm_1_dense_1_kernel_max=0.0, elm_2_dense_1_kernel_min=0.0, elm_2_dense_1_kernel_max=0.0,
+                 elm_3_dense_1_kernel_min=0.0, elm_3_dense_1_kernel_max=0.0, elm_1_dense_1_bias_min=0.0,
+                 elm_1_dense_1_bias_max=0.0, elm_2_dense_1_bias_min=0.0, elm_2_dense_1_bias_max=0.0,
+                 elm_3_dense_1_bias_min=0.0, elm_3_dense_1_bias_max=0.0):
+        super(DlnnDump, self).__init__(network_name, scale_min1, scale_max1, scale_min2, scale_max2, conv_1_window,
+                                       conv_2_window,
+                                       conv_3_window, pool_1_size, pool_2_size, elm_1_dense_1_units,
+                                       elm_2_dense_1_units,
+                                       elm_3_dense_1_units, elm_1_dense_1_kernel_min, elm_1_dense_1_kernel_max,
+                                       elm_2_dense_1_kernel_min, elm_2_dense_1_kernel_max, elm_3_dense_1_kernel_min,
+                                       elm_3_dense_1_kernel_max, elm_1_dense_1_bias_min, elm_1_dense_1_bias_max,
+                                       elm_2_dense_1_bias_min, elm_2_dense_1_bias_max, elm_3_dense_1_bias_min,
+                                       elm_3_dense_1_bias_max)
+
+    def build_model(self):
+        from keras import Input
+        from keras.activations import sigmoid
+        from keras.initializers import Zeros
+        from keras.layers import Reshape, Activation, MaxPooling2D, Flatten, Dense, Concatenate
+
+        self.layer['input'] = Input(
+            shape=(self.input_shape,))
+
+        self.layer['pre_scaling'] = Scaling(
+            scale_min1=self.scale_min1,
+            scale_max1=self.scale_max1,
+            scale_min2=self.scale_min2,
+            scale_max2=self.scale_max2,
+            name='pre_scaling')(self.layer['input'])
+
+        self.layer['pre_reshaping'] = Reshape(
+            target_shape=[1, 1, self.input_shape],
+            name='pre_reshaping')(self.layer['pre_scaling'])
+
+        self.layer['pre_tiling'] = Tiling(
+            units=self.input_shape,
+            name='pre_tiling')(self.layer['pre_reshaping'])
+
+        self.layer['cnn_conv_1'] = Conv2D(
+            filters=[AvgFilter(), MaxFilter(), StdDevFilter()],
+            window=self.conv_1_window,
+            padding='same',
+            use_bias=False,
+            kernel_size=(self.input_shape, self.input_shape),
+            data_format='channels_first',
+            name='cnn_conv_1')(self.layer['pre_tiling'])
+
+        self.layer['cnn_activation_1'] = Activation(
+            activation=sigmoid,
+            name='cnn_activation_1')(self.layer['cnn_conv_1'])
+
+        self.layer['cnn_conv_2'] = Conv2D(
+            filters=[AvgFilter(), MaxFilter(), StdDevFilter()],
+            window=self.conv_2_window,
+            padding='same',
+            use_bias=False,
+            kernel_size=(self.input_shape, self.input_shape),
+            data_format='channels_first',
+            name='cnn_conv_2')(self.layer['cnn_activation_1'])
+
+        self.layer['cnn_activation_2'] = Activation(
+            activation=sigmoid,
+            name='cnn_activation_2')(self.layer['cnn_conv_2'])
+
+        self.layer['cnn_pooling_1'] = MaxPooling2D(
+            pool_size=(self.pool_1_size, self.pool_1_size),
+            padding='same',
+            data_format='channels_first',
+            name='cnn_pooling_1')(self.layer['cnn_activation_2'])
+
+        self.layer['cnn_conv_3'] = Conv2D(
+            filters=[AvgFilter(), MaxFilter(), StdDevFilter()],
+            window=self.conv_3_window,
+            padding='same',
+            use_bias=False,
+            kernel_size=(self.input_shape, self.input_shape),
+            data_format='channels_first',
+            name='cnn_conv_3')(self.layer['cnn_pooling_1'])
+
+        self.layer['cnn_activation_3'] = Activation(
+            activation=sigmoid,
+            name='cnn_activation_3')(self.layer['cnn_conv_3'])
+
+        self.layer['cnn_pooling_2'] = MaxPooling2D(
+            pool_size=(self.pool_2_size, self.pool_2_size),
+            padding='same',
+            data_format='channels_first',
+            name='cnn_pooling_2')(self.layer['cnn_activation_3'])
+
+        self.layer['bridge_flatten'] = Flatten(
+            name='bridge_flatten')(self.layer['cnn_pooling_2'])
+
+        self.layer['elm_1_dense_1'] = Dense(
+            units=self.elm_1_dense_1_units,
+            activation=None,
+            use_bias=True,
+            kernel_initializer=CustomInitializer(corr_step_10_a_kernel_init),
+            bias_initializer=CustomInitializer(corr_step_10_a_bias_init),
+            trainable=False,
+            name='elm_1_dense_1')(self.layer['bridge_flatten'])
+
+        self.layer['elm_1_activation_1'] = Activation(
+            activation=sigmoid,
+            name='elm_1_activation_1')(self.layer['elm_1_dense_1'])
+
+        self.layer['elm_1_dense_2'] = Dense(
+            units=self.category_num,
+            activation=None,
+            use_bias=False,
+            kernel_initializer=Zeros(),
+            trainable=False,
+            name='elm_1_dense_2')(self.layer['elm_1_activation_1'])
+
+        self.layer['elm_2_dense_1'] = Dense(
+            units=self.elm_2_dense_1_units,
+            activation=None,
+            use_bias=True,
+            kernel_initializer=CustomInitializer(corr_step_10_b_kernel_init),
+            bias_initializer=CustomInitializer(corr_step_10_b_bias_init),
+            trainable=False,
+            name='elm_2_dense_1')(self.layer['bridge_flatten'])
+
+        self.layer['elm_2_activation_1'] = Activation(
+            activation=sigmoid,
+            name='elm_2_activation_1')(self.layer['elm_2_dense_1'])
+
+        self.layer['elm_2_dense_2'] = Dense(
+            units=self.category_num,
+            activation=None,
+            use_bias=False,
+            kernel_initializer=Zeros(),
+            trainable=False,
+            name='elm_2_dense_2')(self.layer['elm_2_activation_1'])
+
+        self.layer['elm_3_dense_1'] = Dense(
+            units=self.elm_3_dense_1_units,
+            activation=None,
+            use_bias=True,
+            kernel_initializer=CustomInitializer(corr_step_10_c_kernel_init),
+            bias_initializer=CustomInitializer(corr_step_10_c_bias_init),
+            trainable=False,
+            name='elm_3_dense_1')(self.layer['bridge_flatten'])
+
+        self.layer['elm_3_activation_1'] = Activation(
+            activation=sigmoid,
+            name='elm_3_activation_1')(self.layer['elm_3_dense_1'])
+
+        self.layer['elm_3_dense_2'] = Dense(
+            units=self.category_num,
+            activation=None,
+            use_bias=False,
+            kernel_initializer=Zeros(),
+            trainable=False,
+            name='elm_3_dense_2')(self.layer['elm_3_activation_1'])
+
+        self.layer['fully_connected_concat'] = Concatenate(
+            name='fully_connected_concat')(
+            [self.layer['elm_1_dense_2'], self.layer['elm_2_dense_2'], self.layer['elm_3_dense_2']])
+
+        self.layer['fully_connected_reshape'] = Reshape(
+            target_shape=(self.fully_connected_num, self.category_num),
+            name='fully_connected_reshape')(self.layer['fully_connected_concat'])
+
+        self.layer['fully_connected_merge'] = MergeCategorical(
+            categorical_length=self.category_num,
+            name='fully_connected_merge')(self.layer['fully_connected_reshape'])
+
+    def get_model(self):
+        return load_model(self.network_path, custom_objects={'Scaling': Scaling,
+                                                             'Tiling': Tiling,
+                                                             'AvgFilter': AvgFilter,
+                                                             'MaxFilter': MaxFilter,
+                                                             'StdDevFilter': StdDevFilter,
+                                                             'Conv2D': Conv2D,
+                                                             'MergeCategorical': MergeCategorical,
+                                                             'Unifinv': Unifinv,
+                                                             'CustomInitializer': CustomInitializer})
 
 
 class DlnnFunctionalTest(TestCase):
